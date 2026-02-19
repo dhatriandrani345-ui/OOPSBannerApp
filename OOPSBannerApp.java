caa69006d84393fc4c7be6f6a6f9489c4f0674aa
dhatriandrani345-ui <dhatriandrani.345@gmail.com>
2026-02-19 10:31:54 +0530

    uc1 completed

---
+++ OOPSBannerApp.java
@@ -0,0 +1,6 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class OOPSBannerApp {
+   public static void main(String[] var0) {
+      System.out.println("OOPS");
+   }
+}
